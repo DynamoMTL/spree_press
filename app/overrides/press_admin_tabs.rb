49949336a817b0bc797bba68d0e7d9fffadeb0c4
Josh Nussbaum <joshnuss@gmail.com>
2014-09-15 23:41:32 -0400

    Updated admin tabs override virtual_path

--- app/overrides/press_admin_tabs.rb
+++ app/overrides/press_admin_tabs.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "press_clippings_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab(:press_clippings) %>")
