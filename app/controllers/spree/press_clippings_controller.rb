7b5b2e7287a3cf816fc829bcdfaabacc3c868f9e
Bryan Mahoney <bry@godynamo.com>
2015-04-24 09:27:14 -0400

    Update press_clippings_controller.rb

--- app/controllers/spree/press_clippings_controller.rb
+++ app/controllers/spree/press_clippings_controller.rb
@@ -5,10 +5,12 @@ module Spree
 
     def index
       @press_clippings = Spree::PressClipping.order('id desc')
+      respond_with(@press_clippings)
     end
 
     def show
       @press_clipping = Spree::PressClipping.find params[:id]
+      respond_with(@press_clipping)
     end
   end
 end
